9ce3ce1afe9ffd6e16edfb8d8396bf05116e2c6a
Tamar <tamar75262@gmail.com>
2026-07-07 14:06:00 +0300

    the project is running

---
+++ server/server.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main()
+{
+    int server_fd, client_fd;
+    struct sockaddr_in server_addr;
+    char buffer[1024] = {0};
+
+    // 1. יצירת socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1)
+    {
+        perror("socket");
+        return 1;
+    }
+
+    // 2. הגדרת השרת
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    // 3. bind
+    int result = bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (result == -1)
+    {
+        perror("bind");
+        return 1;
+    }
+
+    // 4. listen
+    result = listen(server_fd, 5);
+    if (result == -1)
+    {
+        perror("listen");
+        return 1;
+    }
+
+    printf("Server is running...\n");
+
+    // 5. accept
+    client_fd = accept(server_fd, NULL, NULL);
+    if (client_fd == -1)
+    {
+        perror("accept");
+        return 1;
+    }
+
+    printf("Client connected!\n");
+
+    while (1)
+    {
+        // איפוס הבאפר לפני כל קריאה חדשה כדי שלא יישארו שאריות מהקלט הקודם
+        memset(buffer, 0, sizeof(buffer));
+
+        // 6. recv
+        result = read(client_fd, buffer, 1024);
+        if (result == -1)
+        {
+            perror("read");
+            return 1;
+        }
+        else if (result == 0)
+        {
+            printf("Client disconnected.\n");
+            break;
+        }
+
+        printf("Client says: %s\n", buffer);
+
+        // 7. send response
+        result = send(client_fd, buffer, strlen(buffer), 0);
+        if (result == -1)
+        {
+            perror("send");
+            return 1;
+        }
+    }
+
+    // 8. close
+    printf("Closing connections...\n");
+    close(client_fd);
+    close(server_fd);
+
+    return 0;
+}
